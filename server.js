d55e51989764c26617868d4af589df6716bc9278
Austin WIley <wileybeer@gmail.com>
2019-03-22 16:25:17 -0600

    add mongo connection to Sever

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
 // const apiRoutes = require("./routes/apiRoutes");
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactbooklist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/hyperlocal");
 
 // Define any API routes before this runs
 app.get("*", function(req, res) {
