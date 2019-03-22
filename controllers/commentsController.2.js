eb4a8de41f70c1104c5e3be7ae663af5c49728fd
Daniel Duffy <dpd1208@gmail.com>
2019-03-21 20:13:13 -0600

    controllers

--- controllers/commentsController.2.js
+++ controllers/commentsController.2.js
@@ -3,32 +3,32 @@ const db = require("../models");
 // Defining methods for the userController
 module.exports = {
   findAll: function(req, res) {
-    db.Post
+    db.Comment
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Post
+    db.Comment
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Post
+    db.Comment
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Post
+    db.Comment
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Post
+    db.Comment
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
