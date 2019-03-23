5d05041d6fd3c4bb62435c9d321517cb840a27e1
Mary <pocketdru@gmail.com>
2019-03-23 14:12:37 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+    return (
+        <footer className="nav footer">
+        <div className="content has-text-centered">
+         <h1>hyperlocal</h1>
+        </div>
+      </footer>
+    );
+  }
+
+  export default Nav;
