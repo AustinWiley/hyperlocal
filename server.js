7d5d932c15cbaf4723b159625f8e25a6ae4492b9
Austin WIley <wileybeer@gmail.com>
2019-04-05 17:40:11 -0600

    server routes fix

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Static file declaration
-// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Use apiRoutes
 app.use(routes);
 
 //production mode
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join('client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   //
   app.get('*', (req, res) => {
     res.sendfile(path.join(__dirname = 'client/build/index.html'));
