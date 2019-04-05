796258f4d98987044da7b2b55f822380bd532f70
Austin WIley <wileybeer@gmail.com>
2019-04-05 14:45:05 -0600

    commit

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.use(express.json());
 //Static file declaration
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+// Connect to the Mongo DB { useNewUrlParser: true }
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/hyperlocal");
+
+// Use apiRoutes
+app.use(routes);
+
 //production mode
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
@@ -24,12 +30,6 @@ if(process.env.NODE_ENV === 'production') {
     res.sendfile(path.join(__dirname = 'client/build/index.html'));
   })
 }
-
-// Use apiRoutes
-app.use(routes);
-
-// Connect to the Mongo DB { useNewUrlParser: true }
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/hyperlocal");
 
 //build mode
 app.get('*', (req, res) => {
