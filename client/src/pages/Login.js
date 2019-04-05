20a53715cf452f2ae8c9164251cd040fad204c65
Austin WIley <wileybeer@gmail.com>
2019-04-05 12:55:19 -0600

    statefull login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,19 +1,15 @@
-import React, {Component} from "react";
+import React from "react";
 // import React, {Component} from "react";
 // import { Col, Row, Container } from "../components/Grid";
 import Welcome from "../components/Welcome"
 
-export default class Login extends Component {
-    state = {
-        UserInfo: [],
-         user: this.props.name
-       };
-     
-    render() {
-      return (
-         //    <Container fluid>
-            <Welcome onClick={this.props.auth.login}/>
-         //    </Container>
-        )
-    }
-}
+function Login(props) {
+   return (
+    //    <Container fluid>
+
+       <Welcome onClick={props.auth.login}/>
+    //    </Container>
+   )
+};
+  
+  export default Login;
