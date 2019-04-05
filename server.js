d581ccd3bf612cb16fdfbcd3a83fc26a61b26412
Austin WIley <wileybeer@gmail.com>
2019-04-05 17:51:17 -0600

    server file edit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Static file declaration
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
 //production mode
 if(process.env.NODE_ENV === 'production') {
