a65d48f604335803132eb06546fe6e211a5eba54
Daniel Duffy <dpd1208@gmail.com>
2019-04-04 21:39:47 -0600

    auth

--- client/src/Auth.js
+++ client/src/Auth.js
@@ -9,7 +9,7 @@ export default class Auth {
     auth0 = new auth0.WebAuth({
         domain: "reallycoolauth.auth0.com",
         clientID: "2SX696cmEE1CmSS1n1q7gqXjJLV9Ut8X",
-        redirectUri:"http://localhost:3000/callback",
+        redirectUri: window.location.origin + "/callback",
         audience: "https://reallycoolauth.auth0.com/userinfo",
         responseType: "token id_token",
         scope: "openid email profile"
